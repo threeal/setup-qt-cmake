67f6591124aacd4879a81f6d467bdc5fdf72cf7c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-11 17:00:24 +0700

    test: build example from cloned repository (#12)
    * test: modify test to build example from cloned repository
    
    * ci(test): modify install Qt step not to include examples
    
    * ci(test): modify install Qt step to install to default dir

--- test/BuildExampleTest.cmake
+++ test/BuildExampleTest.cmake
@@ -8,12 +8,46 @@ set(TEST_COUNT 0)
 if("Build analogclock example" MATCHES ${TEST_MATCHES})
   math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
 
+  if(NOT EXISTS qtbase)
+    message(STATUS "Cloning analogclock project")
+    execute_process(
+      COMMAND git clone --filter=blob:none --no-checkout https://github.com/qt/qtbase
+      RESULT_VARIABLE RES
+    )
+    if(NOT RES EQUAL 0)
+      message(FATAL_ERROR "Failed to clone analogclock project")
+    endif()
+  endif()
+
+  message(STATUS "Checking out analogclock project")
+  execute_process(
+    COMMAND git -C qtbase sparse-checkout set --cone
+    RESULT_VARIABLE RES
+  )
+  if(NOT RES EQUAL 0)
+    message(FATAL_ERROR "Failed to check out analogclock project")
+  endif()
+  execute_process(
+    COMMAND git -C qtbase checkout 6.2.4
+    RESULT_VARIABLE RES
+  )
+  if(NOT RES EQUAL 0)
+    message(FATAL_ERROR "Failed to check out analogclock project")
+  endif()
+  execute_process(
+    COMMAND git -C qtbase sparse-checkout set examples/gui/analogclock examples/gui/rasterwindow
+    RESULT_VARIABLE RES
+  )
+  if(NOT RES EQUAL 0)
+    message(FATAL_ERROR "Failed to check out analogclock project")
+  endif()
+
   message(STATUS "Reconfiguring analogclock project")
   execute_process(
     COMMAND ${CMAKE_COMMAND}
-      -B ${CMAKE_CURRENT_LIST_DIR}/build/analogclock
+      -B build/analogclock
       --fresh
-      $ENV{HOME}/Qt/Examples/Qt-6.2.4/gui/analogclock
+      qtbase/examples/gui/analogclock
     RESULT_VARIABLE RES
   )
   if(NOT RES EQUAL 0)
@@ -22,7 +56,7 @@ if("Build analogclock example" MATCHES ${TEST_MATCHES})
 
   message(STATUS "Building analogclock project")
   execute_process(
-    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_LIST_DIR}/build/analogclock
+    COMMAND ${CMAKE_COMMAND} --build build/analogclock
     RESULT_VARIABLE RES
   )
   if(NOT RES EQUAL 0)
