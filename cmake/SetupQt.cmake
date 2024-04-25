a56c42a5c862d332dd793f3f59d8259ee438ece8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-25 23:18:38 +0700

    feat: add `_attach_qt_online_installer` function

--- cmake/SetupQt.cmake
+++ cmake/SetupQt.cmake
@@ -27,3 +27,38 @@ function(_download_qt_online_installer)
 
   set(QT_ONLINE_INSTALLER_PATH ${INSTALLER_PATH} PARENT_SCOPE)
 endfunction()
+
+# Attaches the Qt online installer image to a new volume.
+#
+# This function attaches the Qt online installer DMG image to a new volume.
+# It sets the `QT_ONLINE_INSTALLER_VOLUME_PATH` variable to the location of the attached Qt online installer volume.
+#
+# This function does not do anything if the Qt online installer is not a DMG image.
+function(_attach_qt_online_installer)
+  if(NOT DEFINED QT_ONLINE_INSTALLER_PATH)
+    _download_qt_online_installer()
+  endif()
+
+  get_filename_component(QT_ONLINE_INSTALLER_LAST_EXT ${QT_ONLINE_INSTALLER_PATH} LAST_EXT)
+  if(QT_ONLINE_INSTALLER_LAST_EXT STREQUAL .dmg)
+    get_filename_component(QT_ONLINE_INSTALLER_NAME_WLE ${QT_ONLINE_INSTALLER_PATH} NAME_WLE)
+    set(QT_ONLINE_INSTALLER_VOLUME_PATH /Volumes/${QT_ONLINE_INSTALLER_NAME_WLE})
+
+    if(NOT EXISTS ${QT_ONLINE_INSTALLER_VOLUME_PATH})
+      find_program(HDIUTIL_PROGRAM hdiutil)
+      if(HDIUTIL_PROGRAM STREQUAL HDIUTIL_PROGRAM-NOTFOUND)
+        message(FATAL_ERROR "Could not find the 'hdiutil' program required to attach the Qt online installer")
+      endif()
+
+      execute_process(
+        COMMAND ${HDIUTIL_PROGRAM} attach ${QT_ONLINE_INSTALLER_PATH}
+        RESULT_VARIABLE RES
+      )
+      if(NOT RES EQUAL 0)
+        message(FATAL_ERROR "Failed to attach the Qt online installer image at '${QT_ONLINE_INSTALLER_PATH}' to a new volume (${RES})")
+      endif()
+    endif()
+
+    set(QT_ONLINE_INSTALLER_VOLUME_PATH ${QT_ONLINE_INSTALLER_VOLUME_PATH} PARENT_SCOPE)
+  endif()
+endfunction()
