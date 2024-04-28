d73421f43a0e819c172d320a4e096314036c820c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-28 21:52:32 +0700

    refactpr: modify Qt online installer download, attach, and detach functions (#37)
    * refactor: modify `_download_qt_online_installer` to either set image or program variable
    
    * refactor: modify `_attach_qt_online_installer` to require image variable
    
    * refactor: modify `_detach_qt_online_installer` to require volume variable

--- cmake/SetupQt.cmake
+++ cmake/SetupQt.cmake
@@ -4,7 +4,10 @@
 include_guard(GLOBAL)
 
 # Downloads the Qt online installer to a build directory.
-# This function outputs a `QT_ONLINE_INSTALLER_PATH` variable indicating the location of the downloaded installer.
+#
+# If the downloaded file is a DMG image, this function will set the 'QT_ONLINE_INSTALLER_IMAGE' variable to the
+# location of the DMG image; otherwise, it will set the 'QT_ONLINE_INSTALLER_PROGRAM' variable to the location
+# of the Qt online installer program.
 function(_download_qt_online_installer)
   if(CMAKE_SYSTEM_NAME STREQUAL Windows)
     set(INSTALLER_NAME qt-unified-windows-x64-4.7.0-online.exe)
@@ -28,86 +31,83 @@ function(_download_qt_online_installer)
   )
 
   get_filename_component(INSTALLER_LAST_EXT ${INSTALLER_PATH} LAST_EXT)
-  if(INSTALLER_LAST_EXT STREQUAL .run)
+  if(INSTALLER_LAST_EXT STREQUAL .dmg)
+    set(QT_ONLINE_INSTALLER_IMAGE ${INSTALLER_PATH} PARENT_SCOPE)
+  else()
     file(
       CHMOD ${INSTALLER_PATH}
       PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
     )
+    set(QT_ONLINE_INSTALLER_PROGRAM ${INSTALLER_PATH} PARENT_SCOPE)
   endif()
-
-  set(QT_ONLINE_INSTALLER_PATH ${INSTALLER_PATH} PARENT_SCOPE)
 endfunction()
 
 # Attaches the Qt online installer image to a new volume.
 #
-# This function attaches the Qt online installer DMG image to a new volume.
-# It sets the `QT_ONLINE_INSTALLER_VOLUME_PATH` variable to the location of the attached Qt online installer volume
-# and sets the `QT_ONLINE_INSTALLER_PROGRAM` variable to the location of the Qt online installer executable.
-#
-# This function does not do anything if the Qt online installer is not a DMG image,
-# but it still sets the `QT_ONLINE_INSTALLER_PROGRAM` variable.
+# If the 'QT_ONLINE_INSTALLER_IMAGE' variable is defined, this function will attach the Qt online installer DMG image
+# to a new volume, set the `QT_ONLINE_INSTALLER_VOLUME` variable to the location of the attached Qt online installer
+# volume, and set the `QT_ONLINE_INSTALLER_PROGRAM` variable to the location of the Qt online installer executable.
 function(_attach_qt_online_installer)
-  if(NOT DEFINED QT_ONLINE_INSTALLER_PATH)
-    _download_qt_online_installer()
+  if(NOT DEFINED QT_ONLINE_INSTALLER_IMAGE)
+    message(AUTHOR_WARNING "The 'QT_ONLINE_INSTALLER_IMAGE' variable is not defined, do nothing")
+    return()
   endif()
 
-  get_filename_component(QT_ONLINE_INSTALLER_LAST_EXT ${QT_ONLINE_INSTALLER_PATH} LAST_EXT)
-  if(QT_ONLINE_INSTALLER_LAST_EXT STREQUAL .dmg)
-    get_filename_component(QT_ONLINE_INSTALLER_NAME_WLE ${QT_ONLINE_INSTALLER_PATH} NAME_WLE)
-    set(QT_ONLINE_INSTALLER_VOLUME_PATH /Volumes/${QT_ONLINE_INSTALLER_NAME_WLE})
+  get_filename_component(IMAGE_NAME ${QT_ONLINE_INSTALLER_IMAGE} NAME_WLE)
+  set(VOLUME_PATH /Volumes/${IMAGE_NAME})
 
-    if(NOT EXISTS ${QT_ONLINE_INSTALLER_VOLUME_PATH})
-      find_program(HDIUTIL_PROGRAM hdiutil)
-      if(HDIUTIL_PROGRAM STREQUAL HDIUTIL_PROGRAM-NOTFOUND)
-        message(FATAL_ERROR "Could not find the 'hdiutil' program required to attach the Qt online installer")
-      endif()
-
-      execute_process(
-        COMMAND ${HDIUTIL_PROGRAM} attach ${QT_ONLINE_INSTALLER_PATH}
-        RESULT_VARIABLE RES
-      )
-      if(NOT RES EQUAL 0)
-        message(FATAL_ERROR "Failed to attach the Qt online installer image at '${QT_ONLINE_INSTALLER_PATH}' to a new volume (${RES})")
-      endif()
+  if(NOT EXISTS ${VOLUME_PATH})
+    find_program(HDIUTIL_PROGRAM hdiutil)
+    if(HDIUTIL_PROGRAM STREQUAL HDIUTIL_PROGRAM-NOTFOUND)
+      message(FATAL_ERROR "Could not find the 'hdiutil' program required to attach the Qt online installer")
     endif()
 
-    set(QT_ONLINE_INSTALLER_VOLUME_PATH ${QT_ONLINE_INSTALLER_VOLUME_PATH} PARENT_SCOPE)
-
-    set(
-      QT_ONLINE_INSTALLER_PROGRAM
-      ${QT_ONLINE_INSTALLER_VOLUME_PATH}/${QT_ONLINE_INSTALLER_NAME_WLE}.app/Contents/MacOS/${QT_ONLINE_INSTALLER_NAME_WLE}
-      PARENT_SCOPE
+    execute_process(
+      COMMAND ${HDIUTIL_PROGRAM} attach ${QT_ONLINE_INSTALLER_IMAGE}
+      RESULT_VARIABLE RES
     )
-  else()
-    set(QT_ONLINE_INSTALLER_PROGRAM ${QT_ONLINE_INSTALLER_PATH} PARENT_SCOPE)
+    if(NOT RES EQUAL 0)
+      message(
+        FATAL_ERROR
+        "Failed to attach the Qt online installer image at '${QT_ONLINE_INSTALLER_IMAGE}' to a new volume (${RES})"
+      )
+    endif()
   endif()
+
+  set(QT_ONLINE_INSTALLER_VOLUME ${VOLUME_PATH} PARENT_SCOPE)
+  set(QT_ONLINE_INSTALLER_PROGRAM ${VOLUME_PATH}/${IMAGE_NAME}.app/Contents/MacOS/${IMAGE_NAME} PARENT_SCOPE)
 endfunction()
 
 # Detaches the Qt online installer image from the volume.
 #
-# This function detaches the Qt online installer DMG image from the volume.
-# It unsets the `QT_ONLINE_INSTALLER_VOLUME_PATH` and `QT_ONLINE_INSTALLER_PROGRAM` variables.
-#
-# This function does not do anything if the DMG image was not previously attached.
+# If the 'QT_ONLINE_INSTALLER_VOLUME' variable is defined, this function will detach the Qt online installer DMG image
+# from the volume and unset the 'QT_ONLINE_INSTALLER_VOLUME' and 'QT_ONLINE_INSTALLER_PROGRAM' variables.
 function(_detach_qt_online_installer)
-  if(DEFINED QT_ONLINE_INSTALLER_VOLUME_PATH)
-    if(EXISTS ${QT_ONLINE_INSTALLER_VOLUME_PATH})
-      find_program(HDIUTIL_PROGRAM hdiutil)
-      if(HDIUTIL_PROGRAM STREQUAL HDIUTIL_PROGRAM-NOTFOUND)
-        message(FATAL_ERROR "Could not find the 'hdiutil' program required to detach the Qt online installer")
-      endif()
-
-      execute_process(
-        COMMAND ${HDIUTIL_PROGRAM} detach ${QT_ONLINE_INSTALLER_VOLUME_PATH}
-        RESULT_VARIABLE RES
-      )
-      if(NOT RES EQUAL 0)
-        message(FATAL_ERROR "Failed to detach the Qt online installer image from '${QT_ONLINE_INSTALLER_VOLUME_PATH}' (${RES})")
-      endif()
-    endif()
-
-    unset(QT_ONLINE_INSTALLER_VOLUME_PATH PARENT_SCOPE)
+  if(NOT DEFINED QT_ONLINE_INSTALLER_VOLUME)
+    message(AUTHOR_WARNING "The 'QT_ONLINE_INSTALLER_VOLUME' variable is not defined, do nothing")
+    return()
   endif()
 
-  unset(QT_ONLINE_INSTALLER_PROGRAM PARENT_SCOPE)
+  if(EXISTS ${QT_ONLINE_INSTALLER_VOLUME})
+    find_program(HDIUTIL_PROGRAM hdiutil)
+    if(HDIUTIL_PROGRAM STREQUAL HDIUTIL_PROGRAM-NOTFOUND)
+      message(FATAL_ERROR "Could not find the 'hdiutil' program required to detach the Qt online installer")
+    endif()
+
+    execute_process(
+      COMMAND ${HDIUTIL_PROGRAM} detach ${QT_ONLINE_INSTALLER_VOLUME}
+      RESULT_VARIABLE RES
+    )
+    if(NOT RES EQUAL 0)
+      message(
+        FATAL_ERROR
+        "Failed to detach the Qt online installer image from '${QT_ONLINE_INSTALLER_VOLUME}' (${RES})"
+      )
+    endif()
+  endif()
+
+  unset(QT_ONLINE_INSTALLER_VOLUME PARENT_SCOPE)
+  if(QT_ONLINE_INSTALLER_PROGRAM MATCHES "^${QT_ONLINE_INSTALLER_VOLUME}")
+    unset(QT_ONLINE_INSTALLER_PROGRAM PARENT_SCOPE)
+  endif()
 endfunction()
