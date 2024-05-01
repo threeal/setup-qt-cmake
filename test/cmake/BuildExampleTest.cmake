18db136a55bf09b020cb1cf363341c30b8f4803f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-01 22:45:24 +0700

    test: modify build example test to use Qt from `setup_qt` function

--- test/cmake/BuildExampleTest.cmake
+++ test/cmake/BuildExampleTest.cmake
@@ -5,42 +5,47 @@ endif()
 
 set(TEST_COUNT 0)
 
+include(SetupQt)
+
 file(
   DOWNLOAD https://threeal.github.io/git-checkout-cmake/v1.0.0 GitCheckout.cmake
   EXPECTED_MD5 3f49e8e2318773971d21adb98aa24470
 )
 include(GitCheckout.cmake)
 
-if("Build analogclock example" MATCHES ${TEST_MATCHES})
+if("Build rasterwindow example" MATCHES ${TEST_MATCHES})
   math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
+  setup_qt()
 
   if(NOT EXISTS qtbase)
     git_checkout(
       https://github.com/qt/qtbase
-      REF 6.2.4
-      SPARSE_CHECKOUT examples/gui/analogclock examples/gui/rasterwindow
+      REF 6.5.3
+      SPARSE_CHECKOUT examples/gui/rasterwindow
     )
   endif()
 
-  message(STATUS "Reconfiguring analogclock project")
+  message(STATUS "Reconfiguring rasterwindow project")
   execute_process(
     COMMAND ${CMAKE_COMMAND}
-      -B build/analogclock
+      -B build/rasterwindow
+      -D CMAKE_PREFIX_PATH=${QT_CMAKE_PREFIX_PATH}
       --fresh
-      qtbase/examples/gui/analogclock
+      qtbase/examples/gui/rasterwindow
     RESULT_VARIABLE RES
   )
   if(NOT RES EQUAL 0)
-    message(FATAL_ERROR "Failed to reconfigure analogclock project")
+    message(FATAL_ERROR "Failed to reconfigure rasterwindow project")
   endif()
 
-  message(STATUS "Building analogclock project")
+  message(STATUS "Building rasterwindow project")
   execute_process(
-    COMMAND ${CMAKE_COMMAND} --build build/analogclock
+    COMMAND ${CMAKE_COMMAND} --build build/rasterwindow
     RESULT_VARIABLE RES
   )
   if(NOT RES EQUAL 0)
-    message(FATAL_ERROR "Failed to build analogclock project")
+    message(FATAL_ERROR "Failed to build rasterwindow project")
   endif()
 endif()
 
