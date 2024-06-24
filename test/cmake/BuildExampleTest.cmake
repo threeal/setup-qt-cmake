8139155ba6e13c198beb620b3dfb269953765767
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-24 18:14:27 +0700

    chore: bump Assertion.cmake to version 0.3.0

--- test/cmake/BuildExampleTest.cmake
+++ test/cmake/BuildExampleTest.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(
-  DOWNLOAD https://threeal.github.io/assertion-cmake/v0.2.0
+  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v0.3.0/Assertion.cmake
     ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 4ee0e5217b07442d1a31c46e78bb5fac)
+  EXPECTED_MD5 851f49c10934d715df5d0b59c8b8c72a)
 include(${CMAKE_BINARY_DIR}/Assertion.cmake)
 
 file(
@@ -23,13 +23,11 @@ function("Build analogclock example")
 
   message(STATUS "Reconfiguring analogclock project")
   assert_execute_process(
-    COMMAND "${CMAKE_COMMAND}"
-      -B build/analogclock
-      --fresh
+    "${CMAKE_COMMAND}" -B build/analogclock --fresh
       qtbase/examples/gui/analogclock)
 
   message(STATUS "Building analogclock project")
-  assert_execute_process(COMMAND "${CMAKE_COMMAND}" --build build/analogclock)
+  assert_execute_process("${CMAKE_COMMAND}" --build build/analogclock)
 endfunction()
 
 cmake_language(CALL "${TEST_COMMAND}")
