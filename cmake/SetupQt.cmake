bfa58d58358e8bd190f8d41e2f3a2ac000254b03
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-02 15:35:12 +0700

    fix: utilize `CMAKE_HOST_SYSTEM_NAME` to check system name

--- cmake/SetupQt.cmake
+++ cmake/SetupQt.cmake
@@ -9,17 +9,18 @@ include_guard(GLOBAL)
 # location of the DMG image; otherwise, it will set the 'QT_ONLINE_INSTALLER_PROGRAM' variable to the location
 # of the Qt online installer program.
 function(_download_qt_online_installer)
-  if(CMAKE_SYSTEM_NAME STREQUAL Windows)
+  if(CMAKE_HOST_SYSTEM_NAME STREQUAL Windows)
     set(INSTALLER_NAME qt-unified-windows-x64-4.7.0-online.exe)
     set(INSTALLER_HASH cb2dbc9f8b91a2107406a5cea60a9a6b)
-  elseif(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+  elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL Darwin)
     set(INSTALLER_NAME qt-unified-macOS-x64-4.7.0-online.dmg)
     set(INSTALLER_HASH 40b0c04a94764db4d8ecdf37a7a8436f)
-  elseif(CMAKE_SYSTEM_NAME STREQUAL Linux)
+  elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)
     set(INSTALLER_NAME qt-unified-linux-x64-4.7.0-online.run)
     set(INSTALLER_HASH 3b85d14be6e179649f3eb7b92b50ae86)
   else()
-    message(FATAL_ERROR "Unsupported system to download the Qt online installer: ${CMAKE_SYSTEM_NAME}")
+    message(FATAL_ERROR "Unsupported system to download the Qt online "
+      "installer: ${CMAKE_HOST_SYSTEM_NAME}")
   endif()
   set(INSTALLER_PATH ${CMAKE_SOURCE_DIR}/.deps/${INSTALLER_NAME})
 
