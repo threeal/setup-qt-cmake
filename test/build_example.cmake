7aa885fad4e638beb814ab9311b22a1ba3f3592e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-05 19:49:33 +0700

    test: organize tests (#79)
    * test: run test cases sequentially in same module
    
    * test: shift test declaration to top-level `CMakeLists.txt`
    
    * test: utilize `assertion_add_test` to create CMake test
    
    * ci: run test verbosely

--- test/build_example.cmake
+++ test/build_example.cmake
@@ -1,18 +1,10 @@
-cmake_minimum_required(VERSION 3.5)
-
-file(
-  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v1.0.0/Assertion.cmake
-    ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 1d8ec589d6cc15772581bf77eb3873ff)
-include(${CMAKE_BINARY_DIR}/Assertion.cmake)
-
 file(
   DOWNLOAD https://threeal.github.io/git-checkout-cmake/v1.0.0 GitCheckout.cmake
   EXPECTED_MD5 3f49e8e2318773971d21adb98aa24470
 )
 include(GitCheckout.cmake)
 
-function("Build analogclock example")
+section("it should build analogclock example")
   if(NOT EXISTS qtbase)
     git_checkout(
       https://github.com/qt/qtbase
@@ -30,6 +22,4 @@ function("Build analogclock example")
   section("build analogclock project")
     assert_execute_process("${CMAKE_COMMAND}" --build build/analogclock)
   endsection()
-endfunction()
-
-cmake_language(CALL "${TEST_COMMAND}")
+endsection()
--- test/setup_qt.cmake
+++ test/setup_qt.cmake
@@ -1,14 +1,6 @@
-cmake_minimum_required(VERSION 3.5)
+find_package(SetupQt REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
 
-file(
-  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v1.0.0/Assertion.cmake
-    ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 1d8ec589d6cc15772581bf77eb3873ff)
-include(${CMAKE_BINARY_DIR}/Assertion.cmake)
-
-find_package(SetupQt REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../../cmake)
-
-function("Download Qt online installer")
+section("it should download Qt online installer")
   _download_qt_online_installer()
 
   if(CMAKE_HOST_SYSTEM_NAME STREQUAL Darwin)
@@ -20,9 +12,9 @@ function("Download Qt online installer")
     assert(EXISTS "${QT_ONLINE_INSTALLER_PROGRAM}")
     assert(NOT DEFINED QT_ONLINE_INSTALLER_IMAGE)
   endif()
-endfunction()
+endsection()
 
-function("Attach and detach Qt online installer")
+section("it should attach and detach Qt online installer")
   _download_qt_online_installer()
   _attach_qt_online_installer()
 
@@ -44,9 +36,9 @@ function("Attach and detach Qt online installer")
   else()
     assert(DEFINED QT_ONLINE_INSTALLER_PROGRAM)
   endif()
-endfunction()
+endsection()
 
-function("Execute Qt online installer")
+section("it should execute Qt online installer")
   _download_qt_online_installer()
   if(DEFINED QT_ONLINE_INSTALLER_IMAGE)
     _attach_qt_online_installer()
@@ -60,6 +52,4 @@ function("Execute Qt online installer")
   if(DEFINED QT_ONLINE_INSTALLER_VOLUME)
     _detach_qt_online_installer()
   endif()
-endfunction()
-
-cmake_language(CALL "${TEST_COMMAND}")
+endsection()
